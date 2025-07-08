1c6d28689d274fdd00f6f980373ce17bd347c9df
Hiamsnhu Sharma <hsharma114@gmail.com>
2025-07-08 22:13:31 +0530

    Delete API & GET cart

--- src/main/java/in/him/Foodies_App_Be/controller/CartController.java
+++ src/main/java/in/him/Foodies_App_Be/controller/CartController.java
@@ -6,10 +6,7 @@ import in.him.Foodies_App_Be.service.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
@@ -29,5 +26,15 @@ public class CartController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Food Id not found");
         }
         return cartService.addToCart(request);
+    }
+    @GetMapping
+    public CartResponse getCart() {
+        return cartService.getCart();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public  void clearCart() {
+        cartService.clearCart();
     }
 }
--- src/main/java/in/him/Foodies_App_Be/service/CartService.java
+++ src/main/java/in/him/Foodies_App_Be/service/CartService.java
@@ -5,4 +5,6 @@ import in.him.Foodies_App_Be.io.CartResponse;
 
 public interface CartService {
     CartResponse addToCart(CartRequest request);
+    CartResponse getCart();
+    void clearCart();
 }
